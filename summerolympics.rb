db6cbe45d4a0b34dd23a7409c62adfec9ea894bd
JerkaylaN <jerkayla2001@gmail.com>
2018-06-20 19:35:54 +0000

    automatically backed up by learn

--- summerolympics.rb
+++ summerolympics.rb
@@ -12,6 +12,6 @@ summer_olympics[place] =year
 puts summer_olympics
 
 summer_olympics.each do |place|
-  puts "The #{place} summer olympics took place in #{year}"
+  #puts "The #{place} summer olympics took place in #{year}"
   
 end
