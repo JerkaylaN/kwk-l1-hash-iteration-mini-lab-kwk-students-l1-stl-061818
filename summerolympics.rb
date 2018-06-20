0c857a3cb56fe83f5846095a336e4f8ba1b52d9f
JerkaylaN <jerkayla2001@gmail.com>
2018-06-20 19:32:36 +0000

    automatically backed up by learn

--- summerolympics.rb
+++ summerolympics.rb
@@ -1,3 +1,21 @@
+# summer_olympics = { 
+#   "Sydney" => 2000,
+#   "Athens" => 2004,
+#   "Beijing" => 2008,
+#   "London" => 2012
+# }
+
+# place= "Atlanta"
+# year = 1996
+
+# summer_olympics[place] =year
+
+# summer_olympics.each do |place|
+#   puts summer_olympics
+  
+  
+# end
+def summer_olympics
 summer_olympics = { 
   "Sydney" => 2000,
   "Athens" => 2004,
@@ -5,14 +23,4 @@ summer_olympics = {
   "London" => 2012
 }
 
-place= "Atlanta"
-year = 1996
 
-#summer_olympics[place] =year
-#puts summer_olympics
-
-summer_olympics.each do |place|
-  puts summer_olympics.keys
-  
-  
-end
